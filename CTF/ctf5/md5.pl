e9d0c0bff777b18ac8d610aba8e6ce5ac603aa1b
Jose Peralta <jpperaltac@gmail.com>
2019-08-30 21:08:11 -0400

    pWnOS1-0 CTF Completed

---
+++ CTF/ctf5/md5.pl
@@ -0,0 +1,21 @@
+use Digest::MD5 qw(md5 md5_hex md5_base64);
+use DBI;
+
+my $dbh = DBI->connect('DBI:mysql:rainbow','root','') || die "Could not connect $DBI::errstr";
+
+open(FILE, 'rockyou.txt') || die('Could not open file');
+
+while(<FILE>){
+	my $data;
+	chomp($data = $_);
+	$data =~ s/\r\n?//g;
+	$hash = md5_hex $data;
+	$data =~ s/'/''/g;
+	my @vals = ($data, $hash);
+	my $sth = $dbh->prepare("insert into hash(hash_word, hash_hash) values(?,?);");
+	$sth->execute(@vals) || die "Query failed! $DBI::errstr";
+
+}
+
+close(FILE);
+$dbh->disconnect();
